82cc093aba022d9c0165759dbe991102ae93b9f7
Hilla <hilla.abramov@gmail.com>
2024-08-31 21:59:46 +0100

    added format_gbp_currency static function to DataPreparation class

--- utils.py
+++ utils.py
@@ -12,3 +12,16 @@ class DataPrepation:
 		self.gbp_value_columeself.gbp_value_columns = ['Weekend Gross', 'Site average', 'Total Gross to date']
        		self.percentage_value_columns = ["% change on last week"] # allowing for more to be added later on
 
+	@staticmethod
+	def format_gbp_currency(value: int) --> str:
+		str_value = str(value)
+		
+		def insert_commas(str_value, counter):
+			if len(str_value) <= 3:
+				return str_value
+			
+			result = insert_commas(str_value[:-3, counter + 3)
+			return result + "," + str_value[-3:]
+
+		str_value_with_commas = insert_commas(str_value, counter=0)
+		return "£" + str_value_with_commas
