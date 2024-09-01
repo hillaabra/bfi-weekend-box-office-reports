22c220b34e4ef3c02bcaa4c7f09a0fed95c328b9
Hilla <hilla.abramov@gmail.com>
2024-09-01 17:17:10 +0100

    added methods to read notes on top 15 to list

--- excel_parser.py
+++ excel_parser.py
@@ -24,6 +24,8 @@ class ExcelParser:
       self.start_boundary_of_comments_on_top_15_section = self._find_start_boundary_of_comments_on_top_15()
       self.start_boundary_of_notes_on_top_15_section = self._find_start_boundary_of_notes_for_top_15_section()
       self.list_of_comments_on_top_15_result = self._read_comments_on_top_15_to_list()
+      self.start_boundary_of_openers_next_week_table = self._find_start_of_openers_next_week_table()
+      self.list_of_notes_on_top_15_table = self._read_notes_for_top_15_table_to_list()
 
     def _read_top_15_table_to_df(self):
       df_top15 = pd.read_excel(self.workbook, header=1, nrows=15)
@@ -113,8 +115,26 @@ class ExcelParser:
     def _read_comments_on_top_15_to_list(self) -> list:
        row_index_section_start = self.start_boundary_of_comments_on_top_15_section
        row_index_section_end = self.start_boundary_of_notes_on_top_15_section
-       notes_list = []
+       comments_list = []
        for row_index in range(row_index_section_start, row_index_section_end):
+          if self._check_for_empty_row(row_index) == False:
+             comments_list.append(self.excel_sheet.cell_value(row_index, 1)) # expects the value to be in column B
+       return comments_list
+
+    def _find_start_of_openers_next_week_table(self) -> int:
+       for row_index in range(self.start_boundary_of_notes_on_top_15_section, self.excel_sheet.nrows):
+         row = self.excel_sheet.row_values(row_index)
+         if "Openers next week:" in row:
+            return row_index
+       raise ValueError("Expected to find header 'Openers next week:' in workbook sheet after\
+                       end of 'Notes for Top 15 table' section. String not found in search of rows. \
+                        Check document layout.")
+
+    def _read_notes_for_top_15_table_to_list(self) -> list:
+       row_index_section_start = self.start_boundary_of_notes_on_top_15_section
+       row_index_section_end = self.start_boundary_of_openers_next_week_table
+       notes_list = []
+       for row_index in range(row_index_section_start + 1, row_index_section_end):
           if self._check_for_empty_row(row_index) == False:
              notes_list.append(self.excel_sheet.cell_value(row_index, 1)) # expects the value to be in column B
        return notes_list
@@ -138,4 +158,4 @@ if __name__ == "__main__":
    # print("Total Weekend Gross: ", excel_parser.total_top_15_weekend_gross)
    # print("Total Gross to date: ", excel_parser.total_top_15_gross_to_date)
    # print("Other UK Films df: ", excel_parser.other_uk_films_df)
-   print(excel_parser.start_boundary_of_notes_on_top_15_section)
+   print(excel_parser.list_of_notes_on_top_15_table)
